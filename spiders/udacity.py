18cb9d26ae4dff2e5311332936e3d622ffcc8b56
rpdswtk <szvacsi@gmail.com>
2020-10-11 15:05:10 +0200

    Create udacity sipder

--- spiders/udacity.py
+++ spiders/udacity.py
@@ -1,92 +1,56 @@
 import json
-import re
-import os
-import time 
+import requests
+import scrapy
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 
-def format_stars(s):
-	starting = s.index(':')
-	ending = s.index('%')
-	answer = s[starting + 1:ending]
-	answer = answer.strip()
-	answer = float(answer) / 20
-	answer = round(answer, 3)
-	return answer
+BASE_URL = "https://www.udacity.com/courses/all"
 
-def configure_driver():
-	chrome_options = Options()
-	chrome_options.add_argument("--headless")
-	# chrome_options = Options()
-	chrome_options.add_argument("--disable-dev-shm-usage")
-	chrome_options.add_argument("--no-sandbox")
-	# driver = webdriver.Chrome(executable_path="chromedriver.exe", options = chrome_options)
-	driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options = chrome_options)
-	return driver
+class UdacitySpider(scrapy.Spider):
+    name = "udacity_spider"
 
-def getCourses(driver , search_keyword):
-	driver.get(f"https://www.udacity.com/courses/all?search={search_keyword}")
-	try:
-		WebDriverWait(driver, 5).until(lambda s: s.find_element_by_class_name("catalog-cards__list").is_displayed())
-		ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-	except TimeoutException:
-		print("TimeoutException: Element not found")
+    def __init__(self, *args, **kwargs):
+	    super(UdacitySpider, self).__init__(*args, **kwargs)
+	    self.search_term = kwargs.get('category')
+	    self.start_urls = [BASE_URL]
 
-	buttons = driver.find_elements_by_xpath("//button[@class='card__bottom__button']")
-	i = 0
-	for button in buttons:
-		try:
-			ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-			time.sleep(0.1)
-			button.click()
-			i = i + 1
-			if(i == 20):
-				break
-		except:
-			pass
-	ActionChains(driver).send_keys(Keys.ESCAPE).perform()
-	
-	soup = BeautifulSoup(driver.page_source, "lxml")
+    def parse(self, response):
+        page = BeautifulSoup(response.body, 'lxml')
+        courses = page.findAll("div", {"class": "catalog-component__card"})
+        for course in courses:
+            link_to_course = "https://www.udacity.com" + course.find('a', {'class': 'card__top'})["href"]
 
-	mylist = []
-	for course_page in soup.select("ul.catalog-cards__list"):
-		for course in course_page.select("li.catalog-cards__list__item"):
-			mydict = {}
+            skills = course.find("p", {"class": "text-content__text"})
+            course_name = course.find("h2", {"class": "card__title__nd-name"}).text
+            if self.search_term.lower() in course_name.lower() or skills and self.search_term.lower() in skills.text.lower():
+                course_id = link_to_course.split("--")[-1]
 
-			title_selector = "article.catalog-component div.catalog-component__card a.card__top div.card__title-container h2"
-			partner_selector = "article.catalog-component div.catalog-component__card a.card__top div.card__title-container h3"
-			author_selector = "article.catalog-component div.catalog-component__card a.card__top div.card__text-content section p.text-content__text"
-			link_selector = "article.catalog-component div.catalog-component__card a.card__top"
-			image_selector = "https://d20vrrgs8k4bvw.cloudfront.net/images/open-graph/udacity.png"
-			level_selector = "article.catalog-component div.catalog-component__card div.card__bottom div.difficulty"
-			reviews_selector = "article.catalog-component div.catalog-component__details div.layout__button-container div.reviews"
-			stars_selector = "article.catalog-component div.catalog-component__details div.layout__button-container div.reviews div.nd-rating-stars div.active-stars"
+                sections = list(course.find('div' , {'class': 'card__text-content'}).children)
+                if len(sections) == 2:
+                    partner_name = sections[1].find('p').text
+                else:
+                    partner_name = None
 
-			if course.select_one(reviews_selector).text:
-				try:
-					mydict["rating_count"] = float(re.sub(" Reviews","",str(course.select_one(reviews_selector).text)))
-					mydict["rating_out_of_five"] = float((format_stars(course.select_one(stars_selector)['style'])))
-					mydict["course_name"] = course.select_one(title_selector).text
-					mydict["offered_by"] = "Udacity"
-					mydict["partner_name"] = course.select(author_selector)[-1].text
-					mydict["link_to_course"] = 'https://www.udacity.com' + course.select_one(link_selector)['href']
-					mydict["image_link"] = image_selector
-					mydict["difficulty_level"] = course.select_one(level_selector).text.capitalize()
-					mylist.append(mydict)
-				except:
-					pass
-			else:
-				continue
-			
-	return mylist
+                image_link = "https://d20vrrgs8k4bvw.cloudfront.net/images/open-graph/udacity.png"
 
-def func(search_keyword):
-	driver = configure_driver()
-	list = getCourses(driver,search_keyword)
-	driver.close()
-	return list
+                difficulty_level = course.find('div', {'class': 'difficulty'}).find('small').text
+
+                ratings_page = requests.get(f"https://ratings-api.udacity.com/api/v1/reviews?node={course_id}")
+                rating_count = 0
+                stats = json.loads(ratings_page.content)["stats"]
+                avg_ratings = 0
+                for stat in stats:
+                    rating_count += stat["count"]
+                    avg_ratings += stat["count"] * stat["rating"]
+                if rating_count > 0:
+                    avg_ratings = avg_ratings / rating_count
+                rating_out_of_five = round(avg_ratings, 1)
+                yield {
+                    'course_name': course_name,
+                    'partner_name': partner_name,
+                    'image_link': image_link,
+                    'rating_out_of_five': rating_out_of_five,
+                    'rating_count': rating_count,
+                    'difficulty_level': difficulty_level,
+                    'link_to_course': link_to_course,
+                    'offered_by': 'Udacity'
+			    }
